bb5789dc5ba6a4c1c95d620526a9dfc751569fe3
alexpetrov1001 <alex.petrov1001@gmail.com>
2019-02-26 00:34:39 +0300

    удалил еще один рабочий файл

---
+++ LessonFirst/LessonFirst/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LessonFirst
+//
+//  Created by Александр Филиппов on 26.02.2019.
+//  Copyright © 2019 Philalex. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
